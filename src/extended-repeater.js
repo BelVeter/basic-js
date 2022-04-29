841f80ed28d39ead8a80d5ef0ad87d6b7039b5ee
BelVeter <95185021+BelVeter@users.noreply.github.com>
2022-04-29 23:21:05 +0200

    feat: extended repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  // console.log(options.additionSeparator)
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  if (options.addition !== undefined) options.addition= String(options.addition);
+  if (options.additionRepeatTimes !== undefined) options.additionRepeatTimes=options.additionRepeatTimes*1;
+  if (options.additionSeparator !== undefined) options.additionSeparator= String(options.additionSeparator);
+  if (options.repeatTimes !== undefined) options.repeatTimes=options.repeatTimes*1;
+  if (options.separator !== undefined) options.separator=String(options.separator);
+
+
+  let addition=makeString(options.addition || '', options.additionRepeatTimes || 1, options.additionSeparator || '|');
+
+  let str2=str+addition;
+
+  rez = makeString(str2, +options.repeatTimes || 1, options.separator || '+');
+
+  // console.log(rez);
+
+  function makeString (str, len, glue) {
+    // console.log(str, len, glue);
+    let arr = new Array(+len).fill(str.toString());
+    return arr.join(glue.toString());
+  }
+
+  return rez;
 }
 
 module.exports = {
   repeater
 };
+
+// console.log(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }))//'TESTstrADD!'
+//'truefalse!!!false??? truefalse!!!false??? truefalse!!!false'
--- src/transform-array.js
+++ src/transform-array.js
@@ -14,9 +14,61 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(/* arr */) {
+  // console.log(arr);
+  // debugger
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  // if(!Array.isArray(arr)) {
+  //   throw new Error("'arr' parameter must be an instance of the Array!");
+  // }
+
+  // let arrCopy=[...arr];
+
+  // let rez = [];
+
+  // for(let i = 0; i < arr.length; i++){
+  //   //let el = arrCopy[i];
+
+  //   if(typeof el == 'string' && el[0] == '-' && el[1]=='-'){
+  //     let tmpArray=el.split('-');
+  //     let command = tmpArray[2];
+  //     let indexText = tmpArray[3];
+
+  //     if(command=='discard') {
+  //       if(indexText=='next') {
+  //         if (i==arr.length-1) continue;
+  //         delete(arrCopy[i+1]);
+  //         i = i+1;
+  //       }
+  //       else {//previous
+  //         if(i==0) continue;
+  //         if(arrCopy[i-1] === undefined) continue;
+  //         rez.pop();
+  //       };
+  //     }
+  //     else{//double
+  //       if(indexText=='next') {
+  //         if (i==arr.length-1) continue;
+  //         rez.push(arr[i+1]);
+  //       }
+  //       else {//previous
+  //         if(i==0) continue;
+  //         if(arrCopy[i-1] !== undefined) rez.push(arr[i-1]);
+  //       }
+  //     }
+  //   }
+  //   else{
+  //     if(el !== undefined) rez.push(el);
+  //   }
+  // }
+
+  // return rez;
+
 }
+
+// console.log(transform([1, 2, 3, '--double-next', 4, 5]));
+// console.log(transform([ 1, '--discrard-prev', true ]));
 
 module.exports = {
   transform
