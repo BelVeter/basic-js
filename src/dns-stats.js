f759b5ae9ae53f700707d31095f1fa02665c0f83
BelVeter <95185021+BelVeter@users.noreply.github.com>
2022-04-30 11:18:32 +0200

    feat: dns stats done

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,10 +22,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let rezDomainsObj = {};
+  
+  domains.forEach((el) => {
+    splitDomain(el);
+  });
+
+  return rezDomainsObj;
+
+  function splitDomain(domain) {
+    let domainArray = domain.split('.'); 
+    domainArray.reverse();
+    domainArray.forEach((element, index) => {
+      if(index===0) domainArray[index] = '.'+domainArray[index];
+      else{
+        domainArray[index] = domainArray[index-1]+'.'+domainArray[index];
+      }      
+    });
+
+    domainArray.forEach(element => {
+      if(rezDomainsObj[element]===undefined) {
+        rezDomainsObj[element]= 1;
+      }
+      else{
+        rezDomainsObj[element] += 1;
+      }
+    });
+    //console.log(rezDomainsObj);
+  }
 }
+
+domains = [
+    'code.yandex.ru',
+    'music.yandex.ru',
+    'yandex.ru'
+   ];
+
+//console.log(getDNSStats(domains));
 
 module.exports = {
   getDNSStats
