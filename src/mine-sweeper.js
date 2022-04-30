889abc404355d37be6bbb6df67fb6c3917260732
BelVeter <95185021+BelVeter@users.noreply.github.com>
2022-04-30 14:00:43 +0200

    feat: mines are done

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = [];
+
+  matrix.forEach((row, rowIndex)=>{
+    result.push([]);
+    row.forEach((cell, index)=>{
+      //console.log(rowIndex,index,result[rowIndex]);
+      result[rowIndex].push(getNumberOfMines(rowIndex, index));
+      //result[rowIndex][index] = getNumberOfMines(rowIndex, index);
+    });
+  });
+
+  return (result);
+
+  // function printArray(ar){
+  //   ar.forEach((el)=>{
+  //     console.log(el)
+  //   });
+  // }
+
+  function getNumberOfMines(rowIndex, index){
+    let count = 0;
+    for(let r=rowIndex-1; r<=rowIndex+1; r++) {
+      if(r<0 || r>matrix.length-1) {
+        continue;
+      }
+      for(let c = index-1; c<=index+1; c++){
+        if(c<0 || c>matrix[0].length-1) continue;
+        if (matrix[r][c] == true && !(r==rowIndex && c==index)) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
 }
 
 module.exports = {
