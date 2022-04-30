d528a7e2b2a3534e46402b2c16b93c796cc6d0e9
BelVeter <95185021+BelVeter@users.noreply.github.com>
2022-04-30 11:51:56 +0200

    feat: mac address solved

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function isMAC48Address(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let hexSymbolsArray = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f'];
+
+  let result = true;
+  let hexDigitsArray = n.split('-');
+  if(hexDigitsArray.length !== 6) return false;
+  hexDigitsArray.forEach((letter)=>{
+    if(!isHex(letter)) result = false;
+  });
+
+  return result;
+
+  function isHex(str){
+    let rez = true;
+    let hexLetters = str.split('');
+    if(hexLetters.length !== 2) return false;
+    hexLetters.forEach((letter) => {
+      // console.log(letter.toLowerCase());
+      // console.log(!hexSymbolsArray.includes(letter.toLowerCase()))
+      if(!hexSymbolsArray.includes(letter.toLowerCase())) rez = false;
+    });
+  
+    return rez;
+  }
+
+  
 }
+
+//console.log(isHex('z3'));
+
+//console.log(isMAC48Address('10-1B-z3-84-45-E6'));
+
+
 module.exports = {
   isMAC48Address
 };
