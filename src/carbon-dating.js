9c2d2419e03600c818aa5d9d7f9fef8fce11861f
BelVeter <95185021+BelVeter@users.noreply.github.com>
2022-05-02 18:04:45 +0200

    feat: carbon activity task solved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(sampleActivity === undefined) return false;
+  if(typeof sampleActivity !== 'string') return false;
+  sampleActivity = sampleActivity*1;
+  if(Number.isNaN(sampleActivity)) return false;
+  if(sampleActivity<=0 || sampleActivity>=15) return false;
+
+  let t = Math.log(MODERN_ACTIVITY/sampleActivity) / (0.693/HALF_LIFE_PERIOD);
+  t = Math.ceil(t);
+  return t;
 }
+
+//console.log(dateSample(1));
 
 module.exports = {
   dateSample
